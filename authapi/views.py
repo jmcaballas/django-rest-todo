6da560bd6a3a33b7b788147568e54728e650237c
Joseph Matthew Caballas <joseph@mahusai.ph>
2022-06-17 18:53:30 +0800

    Create RegisterView

--- authapi/views.py
+++ authapi/views.py
@@ -1,3 +1,19 @@
+from rest_framework import generics
+from rest_framework.response import Response
+
 from django.shortcuts import render
 
-# Create your views here.
+from .serializers import RegisterSerializer, UserSerializer
+
+
+class RegisterView(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            'user': UserSerializer(user, context=self.get_serializer_context()).data,
+            'token': AuthToken.objects.create(user)[1],
+        })
