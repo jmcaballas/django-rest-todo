a4d282c8601bd26391d9b74829f56def93c186fc
Joseph Matthew Caballas <joseph@mahusai.ph>
2022-06-17 18:59:09 +0800

    Create LoginView and AuthTokenSerializer for email as log-in

--- authapi/views.py
+++ authapi/views.py
@@ -1,9 +1,11 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from rest_framework.response import Response
+from knox.models import AuthToken
+from knox.views import LoginView as KnoxLoginView
 
-from django.shortcuts import render
+from django.contrib.auth import login
 
-from .serializers import RegisterSerializer, UserSerializer
+from .serializers import RegisterSerializer, UserSerializer, AuthTokenSerializer
 
 
 class RegisterView(generics.GenericAPIView):
@@ -17,3 +19,14 @@ class RegisterView(generics.GenericAPIView):
             'user': UserSerializer(user, context=self.get_serializer_context()).data,
             'token': AuthToken.objects.create(user)[1],
         })
+
+
+class LoginView(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginView, self).post(request, format=None)
