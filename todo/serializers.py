fc40f75cf7ec378000595daff6d01124738a9fe5
Joseph Matthew Caballas <joseph@mahusai.ph>
2022-06-20 17:34:08 +0800

    Create ToDoSerializer

---
+++ todo/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import ToDo
+
+
+class ToDoSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'title',
+            'description',
+            'due',
+            'completed',
+        )
+        model = ToDo
