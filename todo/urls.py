7ba8277a6966a2a2c7eff8abab6feb8f4b2b5d2c
Joseph Matthew Caballas <joseph@mahusai.ph>
2022-06-20 18:04:36 +0800

    Create url path for ToDoDetail view

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import ToDoList
+from .views import ToDoList, ToDoDetail
 
 urlpatterns = [
     path('', ToDoList.as_view()),
+    path('<int:todo_id>/', ToDoDetail.as_view()),
 ]
