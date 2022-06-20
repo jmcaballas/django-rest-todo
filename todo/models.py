157edfc1107fb5cf5e412e88a28b4fe72e587ce6
Joseph Matthew Caballas <joseph@mahusai.ph>
2022-06-20 17:27:00 +0800

    Create ToDo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
+from unixtimestampfield.fields import UnixTimeStampField
+
 from django.db import models
 
-# Create your models here.
+from authapi.models import CustomUser
+
+
+class ToDo(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    due = UnixTimeStampField(use_numeric=True, default=0.0)
+    completed = models.BooleanField()
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
